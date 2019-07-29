ff22957128e3b0d245289161e25ee716bcc55b2b
陈柏林 <selfcode@163.com>
2019-07-29 17:52:41 +0800

    陈柏林→修改商品展示规则,优先展示圆帅

--- src/main/java/com/weisen/www/code/yjf/shopmall/repository/Rewrite_CommodityRepository.java
+++ src/main/java/com/weisen/www/code/yjf/shopmall/repository/Rewrite_CommodityRepository.java
@@ -39,7 +39,7 @@ public interface Rewrite_CommodityRepository extends JpaRepository<Commodity, Lo
     @Query(value = "SELECT c.id as commodityid,s.id as id,s.model as model,s.specifications as specifications ,s.price as price,c.salevalue as salevalue, i.content as content,s.discount as discount from commodity c LEFT JOIN introduce i ON c.id = i.commodityid LEFT JOIN specifications s ON i.commodityid = s.commodityid where i.content like CONCAT('%',?1,'%') and c.commoditystate = 1 ORDER BY i.other , c.createdate DESC LIMIT ?2,?3", nativeQuery = true)
     List<Product> findAllProductByName(String keyWord, Integer pageNum, Integer pageSize);
 
-    @Query(value = "SELECT c.id as commodityid,c.name as name ,s.fileid as fileid,c.postage as postage,s.id as id,s.model as model,s.specifications as specifications ,s.price as price,c.salevalue as salevalue,s.discount as discount from commodity c LEFT JOIN specifications s ON s.commodityid = c.id  ORDER BY c.createdate DESC LIMIT ?1,?2", nativeQuery = true)
+    @Query(value = "SELECT c.id as commodityid,c.name as name ,s.fileid as fileid,c.postage as postage,s.id as id,s.model as model,s.specifications as specifications ,s.price as price,c.salevalue as salevalue,s.discount as discount from commodity c LEFT JOIN specifications s ON s.commodityid = c.id  ORDER BY s.other DESC, c.createdate DESC LIMIT ?1,?2", nativeQuery = true)
     List<Map<String,Object>> findAllProduct(int pageNum, Integer pageSize);
 
     @Query(value = "SELECT count(*) from commodity c LEFT JOIN introduce i ON c.id = i.commodityid LEFT JOIN specifications s ON i.commodityid = s.commodityid where i.content like CONCAT('%',?1,'%') and c.commoditystate = 1 ", nativeQuery = true)
