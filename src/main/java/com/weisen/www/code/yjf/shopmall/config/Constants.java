127fc2f40f75750785120adc5ddf0e42bc872ee2
伍家成 <569475308@qq.com>
2020-01-14 17:14:06 +0800

    手机号正则

--- src/main/java/com/weisen/www/code/yjf/shopmall/config/Constants.java
+++ src/main/java/com/weisen/www/code/yjf/shopmall/config/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
 
     public static final String SYSTEM_ACCOUNT = "system";
 
-    public static final String REGEX_MOBILE = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+    public static final String REGEX_MOBILE = "^(((13[0-9])|(14[0-9])|(15([0-9]))|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\\d{8})$";
 
     private Constants() {
     }
